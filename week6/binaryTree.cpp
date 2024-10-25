d4e0dfd7cab7ecc6ecb3ce85359af9f3aa7668de
namoi4009 <tangocnam23@gmail.com>
2024-10-25 12:23:07 +0700

    passed all tests

--- week6/binaryTree.cpp
+++ week6/binaryTree.cpp
@@ -48,6 +48,115 @@ void LNRtraversal(BNode<E> * root, void (*f)(E)) {
     }
 }
 
+// Question 3
+template <typename E>
+BNode<E>* findMin(BNode<E>* node) {
+    while (node->left() != nullptr) {
+        node = node->left();
+    }
+    return node;
+}
+
+template <typename E>
+BNode<E>* BSTdelete(BNode<E>* root, E val) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    if (val < root->element()) {
+        root->setLeft(BSTdelete(root->left(), val));
+    }
+    else if (val > root->element()) {
+        root->setRight(BSTdelete(root->right(), val));
+    }
+    else {
+        if (root->left() == nullptr && root->right() == nullptr) {
+            delete root;
+            return nullptr;
+        }
+        else if (root->left() == nullptr) {
+            BNode<E>* temp = root->right();
+            root->setRight(nullptr);
+            delete root;
+            return temp;
+        }
+        else if (root->right() == nullptr) {
+            BNode<E>* temp = root->left();
+            root->setLeft(nullptr);
+            delete root;
+            return temp;
+        }
+        else {
+            BNode<E>* successor = findMin(root->right());
+            root->setElement(successor->element());
+            root->setRight(BSTdelete(root->right(), successor->element()));
+        }
+    }
+    return root;
+}
+
+// Question 4
+template <typename E>
+bool check(BNode<E>* node, E min, E max) {
+    if (!node) return true;
+    if (node->element() < min || node->element() > max)
+        return false;
+    return check(node->left(), min, node->element()) && check(node->right(), node->element(), max);
+}
+
+template <typename E>
+bool isBST(BNode<E>* root) {
+    if (!root) return false;
+    return check(root, numeric_limits<E>::lowest(), numeric_limits<E>::max());
+}
+
+// Question 5
+template <typename E>
+int countNode(BNode<E>* root) {
+    if (!root) return 0;
+    return 1 + countNode(root->left()) + countNode(root->right());
+}
+
+template <typename E>
+bool helper(BNode<E> node, int index, int total) {
+    if (!node) return true;
+    if (index >= total) return false;
+    return helper(node->left(), 2 * index + 1, total) && helper(node->right(), 2 * index + 2, total);
+}
+
+template <typename E>
+bool isComplete(BNode<E>* root) {
+    if (!root) return false;
+    int count = countNode(roots);
+    return helper(root, 0, count);
+}
+
+// Question 6
+template <typename E>
+bool isFull(BNode<E>* root) {
+    if (!root) return false;
+    if (!root->left() && !root->right()) return true;
+    if (root->left() && root->right()) return isFull(root->left()) && isFull(root->right());
+    return false;
+}
+
+// Question 7
+template <typename E>
+void LargestSmaller(BNode<E>* root, E key) {
+    E res = numeric_limits<E>::lowest();
+    BNode<E>* curr = root;
+    while (curr) {
+        if (curr->element() < key) {
+            res = curr->element();
+            curr = curr->right();
+        } else {
+            curr = curr->left();
+        }
+    }
+    if (res != numeric_limits<E>::lowest()) cout << res;
+}
+
+
 int main() {
 
     return 0;
